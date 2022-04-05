456525c5b84fc8b3c7ea7ff60a0a2eac305fedcc
Soner Canki <sonercanki01@gmail.com>
2022-04-06 01:39:53 +0300

    First Unit Test

---
+++ CreatingFirstUnitTest/Helpers.Tests/StringHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Tests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void Removing_WhiteSpaces_AtBeginning_And_AtTheAnd()
+        {
+            //Arrange
+
+            var expression = "     Soner Canki    ";
+            var expected = "Soner Canki";
+
+            //Act
+
+            var result = StringHelper.RemoveExtraSpaces(expression);
+
+            //Assert
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void Removing_WhiteSpaces_InInside()
+        {
+            //Arrange
+
+            var expression = "Soner   Zeynep    Özlem     Tuncay   Alihan";
+            var expected = "Soner Zeynep Özlem Tuncay Alihan";
+
+            //Act
+
+            var result = StringHelper.RemoveExtraSpaces(expression);
+
+            //Assert
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
